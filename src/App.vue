99fffd77c28154b9cdd393eae24b6134cd77e69d
erika jonell <erika.jonell@gmail.com>
2017-09-20 11:04:43 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <ToolTip/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import ToolTip from './components/ToolTip'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    ToolTip
+  }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+</style>
---
+++ src/components/ToolTip.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tool-tip">
+    <p>hi there!</p>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'ToolTip',
+
+}
+</script>
+
+<style lang="sass">
+  .tool-tip
+    font-family: sans-serif
+</style>
